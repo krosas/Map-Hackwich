526c237421f875abf7a27b9ef68136f1bc62079c
Kevin Rosas <rosaskevin044@gmail.com>
2021-03-02 10:41:45 -0600

    Initial Commit

---
+++ Map Hackwich/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  Map Hackwich
+//
+//  Created by kevin rosas on 3/2/21.
+//
+
+import SwiftUI
+import MapKit
+
+struct ContentView: View {
+    @State private var region = MKCoordinateRegion(
+        center: CLLocationCoordinate2D(
+            latitude: 42.15704,
+            longitude: -88.14812),
+        span: MKCoordinateSpan(
+            latitudeDelta: 0.05,
+            longitudeDelta: 0.05)
+    )
+    var body: some View {
+        Map(coordinateRegion: $region)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Map Hackwich/Map_HackwichApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_HackwichApp.swift
+//  Map Hackwich
+//
+//  Created by kevin rosas on 3/2/21.
+//
+
+import SwiftUI
+
+@main
+struct Map_HackwichApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
